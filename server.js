4847e56e52405e9643b221687ad1cc15e884e9b9
Nelson Carvajal <62116622+Nelocode@users.noreply.github.com>
2026-09-08 22:58:41 -0500

    hotfix: configure server.js to serve outcrop directory instead of CopperGiant proposal directory

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const zlib = require('zlib');
 
 const PORT = 80;
-const PUBLIC_DIR = path.join(__dirname, 'proposal');
+const PUBLIC_DIR = path.join(__dirname, 'outcrop');
 const DB_FILE = path.join(__dirname, 'data', 'analytics_db.json');
 
 // Ensure data directory exists
